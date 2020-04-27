213a0928a29017df6dd852b1530cd46820a7d030
AliAzarafza <63294493+AliAzarafza@users.noreply.github.com>
2020-04-27 08:54:55 -0400

    filter part2

--- packetSnifer.py
+++ packetSnifer.py
@@ -6,13 +6,15 @@ from scapy.layers import http
 
 
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=processSniffedPacket, filter="udp")
+    # scapy.sniff(iface=interface, store=False, prn=processSniffedPacket, filter="udp")
+    scapy.sniff(iface=interface, store=False, prn=processSniffedPacket)
 
 
 
 def processSniffedPacket(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        if packet.haslayer(scapy.raw):
+            print(packet[scapy.raw].load)
 
 
 
